adf8446f17193c8b850bf952f948e7c3c34cf427
Jay Spark <JustinConcepc@gmail.com>
2023-08-16 12:34:03 -0400

    Added game manager to MainGame Branch

--- Entity.py
+++
@@ -1,4 +0,0 @@
-class Entity:
-    def __init__(self):
-        self.x = 0
-        self.y = 0
--- main.py
+++
@@ -1,5 +0,0 @@
-from Game import Game
-
-if __name__ == "__main__":
-    game = Game()
-    game.on_execute()
