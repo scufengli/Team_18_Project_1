53cd5b069be9101541e3f7e828aacdf120cceec1
Jay Spark <JustinConcepc@gmail.com>
2023-08-25 22:52:09 -0400

    added feng level to state.

---
+++ data/states/Feng_level/Player.py
@@ -0,0 +1,19 @@
+import pygame, os
+from Settings import *
+from Character import Character
+
+class Player(Character):
+    def __init__(self, x = 0, y = 0):
+        super().__init__(x, y)
+
+        self.lives_offset = 0   
+        self.freeze = False
+        self.counter = 0
+        self.armed = False
+        self.hurt_sound = pygame.mixer.Sound(os.path.join(ROOT_PATH, SOUND_PATH, 'player_hurt.mp3'))
+
+    def is_player(self):
+        return True
+    
+    def update(self, display_surf):
+        super().update(display_surf)
